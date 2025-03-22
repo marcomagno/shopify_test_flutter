67e73013f1e0ec713c66d9a80bf3e8b4cf45bfce
Muccy <muccymac@gmail.com>
2025-03-22 15:52:54 +0100

    Add custom HTTP client overrides for handling bad certificates
    This commit introduces a new class, `MyHttpOverrides`, which extends `HttpOverrides` to customize the behavior of the HTTP client. Specifically, it allows the application to accept bad SSL certificates by implementing a callback in the `createHttpClient` method. This change is aimed at improving the flexibility of network requests during development and testing, particularly when dealing with self-signed certificates. The main function has been updated to include this new override, although it is currently commented out for future use.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_shopify_test/src/app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   // ServiceLocator.instance.kickoff();
+  //HttpOverrides.global = MyHttpOverrides();
   runApp(const App());
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
 }
